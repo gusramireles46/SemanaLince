154a9aabd3e54d60bcd169e3811bcf0ba42a6f29
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2025-04-05 19:59:38 -0600

    adicion del logo de la nasa

--- views/inicio.php
+++ views/inicio.php
@@ -1,8 +1,9 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <a href="#"><img src="assets/images/NASA.png" alt="NASA" style="max-width: 50px; max-height: 50px;"></a>
+        <a class="navbar-brand" href="#">Explorador Espacial</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
@@ -17,10 +18,6 @@
                     <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
         </div>
     </div>
 </nav>
